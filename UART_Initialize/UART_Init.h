7bf336e77f127ffdd9c3cc88da1c2cd7446e26cf
Ramneet Kaur <ramneetkaur.ece92@gmail.com>
2019-09-26 13:15:07 -0500

    Update UART_Init.h

--- UART_Initialize/UART_Init.h
+++ UART_Initialize/UART_Init.h
@@ -6,8 +6,6 @@
  * eg. uart0Init(115200) and rx0buf
  * to print on a uart device uartPrint()
  */
-
-
 #ifndef UART_INIT_H_
 #define UART_INIT_H_
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
